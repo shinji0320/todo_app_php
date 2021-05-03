8b6f4c7b987b41a558fd4914480d0e0a5835efde
shinji0320 <shinji.blue.one@gmail.com>
2021-05-03 14:05:46 +0900

    CSRF

--- work/public/index.php
+++ work/public/index.php
@@ -1,8 +1,13 @@
 <?php
 
+session_start();
+
 define('DSN', 'mysql:host=db;dbname=myapp;charset-utf8mb4');
 define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+
+createToken();
 
 try {
   $pdo = new PDO(
@@ -25,11 +30,45 @@ function h($str) {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
+function createToken(){
+  if (!isset($_SESSION['token'])){
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+  }
+}
+
+function validateToken(){
+  if(
+    empty($_SESSION['token']) ||
+    $_SESSION['token'] !== filter_input(INPUT_POST, 'token')
+  ){
+    exit('Invalid post request');
+  }
+}
+
+function addTodo($pdo){
+  $title = trim(filter_input(INPUT_POST, 'title'));
+  if ($title === ''){
+    return;
+  }
+
+  $stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
+  $stmt->bindValue('title', $title, PDO::PARAM_STR);
+  $stmt->execute();
+}
+
 
 function getTodos($pdo) {
   $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
   $todos = $stmt->fetchAll();
   return $todos;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  validateToken();
+  addTodo($pdo);
+
+  header('Location:' . SITE_URL);
+  exit;
 }
 
 $todos = getTodos($pdo);
@@ -49,6 +88,11 @@ $todos = getTodos($pdo);
 </head>
 <body>
   <h1>Todos</h1>
+
+  <form action="" method="post">
+    <input type="text" name="title" placeholder="Type new todo">
+    <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+  </form>
 
   <ul>
     <?php foreach($todos as $todo): ?>
