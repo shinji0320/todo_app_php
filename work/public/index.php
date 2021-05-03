b1c03be47a38037817226f6a41ecd6798d8c18cf
shinji0320 <shinji.blue.one@gmail.com>
2021-05-03 15:49:53 +0900

    [add]checkboxOnClick打ち消し線が追加

--- work/public/index.php
+++ work/public/index.php
@@ -1,71 +1,25 @@
 <?php
 
-session_start();
-
-define('DSN', 'mysql:host=db;dbname=myapp;charset-utf8mb4');
-define('DB_USER', 'myappuser');
-define('DB_PASS', 'myapppass');
-define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+require_once(__DIR__ . '/../app/config.php');
 
 createToken();
 
-try {
-  $pdo = new PDO(
-    DSN,
-    DB_USER,
-    DB_PASS,
-    [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-      PDO::ATTR_EMULATE_PREPARES => false,
-    ]
-  );
-
-} catch (PDOException $e) {
-  echo $e->getMessage();
-  exit;
-}
-
-function h($str) {
-  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-}
-
-function createToken(){
-  if (!isset($_SESSION['token'])){
-    $_SESSION['token'] = bin2hex(random_bytes(32));
-  }
-}
-
-function validateToken(){
-  if(
-    empty($_SESSION['token']) ||
-    $_SESSION['token'] !== filter_input(INPUT_POST, 'token')
-  ){
-    exit('Invalid post request');
-  }
-}
-
-function addTodo($pdo){
-  $title = trim(filter_input(INPUT_POST, 'title'));
-  if ($title === ''){
-    return;
-  }
-
-  $stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
-  $stmt->bindValue('title', $title, PDO::PARAM_STR);
-  $stmt->execute();
-}
+$pdo = getPdoInstance();
 
 
-function getTodos($pdo) {
-  $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
-  $todos = $stmt->fetchAll();
-  return $todos;
-}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   validateToken();
-  addTodo($pdo);
+  $action = filter_input(INPUT_GET, 'action');
+
+  switch ($action){
+    case 'add':
+      addTodo($pdo);
+      break;
+    case 'toggle':
+      toggleTodo($pdo);
+      break;
+  }
 
   header('Location:' . SITE_URL);
   exit;
@@ -89,7 +43,7 @@ $todos = getTodos($pdo);
 <body>
   <h1>Todos</h1>
 
-  <form action="" method="post">
+  <form action="?action=add" method="post">
     <input type="text" name="title" placeholder="Type new todo">
     <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
   </form>
@@ -97,12 +51,17 @@ $todos = getTodos($pdo);
   <ul>
     <?php foreach($todos as $todo): ?>
       <li>
-        <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+        <form action="?action=toggle" method="post">
+          <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+          <input type="hidden" name="id" value="<?= h($todo->id); ?>">
+          <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+        </form>
         <span class="<?= $todo->is_done ? 'done' : ''; ?>">
           <?= h($todo->title); ?>
         </span>
       </li>
     <?php endforeach; ?>
   </ul>
+  <script src="js/main.js"></script>
 </body>
 </html>
