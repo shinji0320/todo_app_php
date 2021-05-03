e1cdbff77bcb34ccc06f94451990b659154f921e
shinji0320 <shinji.blue.one@gmail.com>
2021-05-03 12:06:48 +0900

    [add]todo_list

--- work/public/index.php
+++ work/public/index.php
@@ -1,10 +1,64 @@
+<?php
+
+define('DSN', 'mysql:host=db;dbname=myapp;charset-utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+
+try {
+  $pdo = new PDO(
+    DSN,
+    DB_USER,
+    DB_PASS,
+    [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+      PDO::ATTR_EMULATE_PREPARES => false,
+    ]
+  );
+
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
+
+function h($str) {
+  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+
+function getTodos($pdo) {
+  $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
+  $todos = $stmt->fetchAll();
+  return $todos;
+}
+
+$todos = getTodos($pdo);
+
+// getTodos();の要素確認
+// var_dump($todos);
+// exit;
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf-8">
   <title>My Todos</title>
+  <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
   <h1>Todos</h1>
+
+  <ul>
+    <?php foreach($todos as $todo): ?>
+      <li>
+        <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+        <span class="<?= $todo->is_done ? 'done' : ''; ?>">
+          <?= h($todo->title); ?>
+        </span>
+      </li>
+    <?php endforeach; ?>
+  </ul>
 </body>
 </html>
