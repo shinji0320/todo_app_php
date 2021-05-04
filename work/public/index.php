ef626828d573f958b9536789e0b6cff0833c11f0
shinji0320 <shinji.blue.one@gmail.com>
2021-05-04 17:24:58 +0900

    formを非同期処理に変更

--- work/public/index.php
+++ work/public/index.php
@@ -22,37 +22,22 @@ $todos =  $todo->getAll();
   <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-  <main>
+  <main data-token="<?= Utils::h($_SESSION['token']); ?>">
     <header>
       <h1>Todos</h1>
-
-      <form action="?action=purge" method="post">
-        <span class="delete">Purge</span>
-        <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
-      </form>
+      <span class="purge">Purge</span>
     </header>
 
-    <form action="?action=add" method="post">
+    <form>
       <input type="text" name="title" placeholder="Type new todo">
-      <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
     </form>
 
     <ul>
       <?php foreach($todos as $todo): ?>
-        <li>
-          <input
-            type="checkbox"
-            data-id="<?= Utils::h($todo->id); ?>"
-            data-token="<?= Utils::h($_SESSION['token']);?>"
-            <?= $todo->is_done ? 'checked' : ''; ?>>
-
+        <li data-id="<?= Utils::h($todo->id); ?>">
+          <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
           <span><?= Utils::h($todo->title); ?></span>
-
-          <form action="?action=delete" method="post" class="delete-form">
-            <span class="delete">x</span>
-            <input type="hidden" name="id" value="<?= Utils::h($todo->id); ?>">
-            <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']); ?>">
-          </form>
+          <span class="delete">x</span>
         </li>
       <?php endforeach; ?>
     </ul>
--- work/public/js/main.js
+++ work/public/js/main.js
@@ -1,14 +1,42 @@
 "use strict";
 
 {
+  const token = document.querySelector('main').dataset.token;
+  const input = document.querySelector('[name="title"]');
+
+  input.focus();
+
+  document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    fetch("?action=add", {
+      method: "POST",
+      body: new URLSearchParams({
+        title: input.value,
+        token: token,
+      }),
+    })
+
+    .then(response => response.json())
+    .then(json => {
+      console.log(json.id);
+    });
+
+    input.value = '';
+    input.focus();
+
+    console.log('Finsh');
+  });
+
+
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   checkboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", () => {
       fetch("?action=toggle", {
         method: "POST",
         body: new URLSearchParams({
-          id: checkbox.dataset.id,
-          token: checkbox.dataset.token,
+          id: checkbox.parentNode.dataset.id,
+          token: token,
         }),
       });
     });
@@ -20,15 +48,36 @@
       if (!confirm("Are you sure?")) {
         return;
       }
-      span.parentNode.submit();
+      fetch("?action=delete", {
+        method: "POST",
+        body: new URLSearchParams({
+          id: span.parentNode.dataset.id,
+          token: token,
+        }),
+      });
+
+      span.parentNode.remove();
     });
   });
 
-  const purge = document.querySelectorAll(".purge");
+  const purge = document.querySelector(".purge");
   purge.addEventListener("click", () => {
     if (!confirm("Are you sure?")) {
       return;
     }
-    purge.parentNode.submit();
+    
+    fetch("?action=purge", {
+      method: "POST",
+      body: new URLSearchParams({
+        token: token,
+      }),
+    });
+
+    const lis = document.querySelectorAll('li');
+    lis.forEach(li => {
+      if (li.children[0].checked) {
+        li.remove();
+      }
+    });
   });
 }
